0b55cb42696e0b99c219ca626abfe5faf6586654
Pranay Ahlawat <pranayahlawat@gmail.com>
2010-01-30 01:30:30 -0500

    adding basic java files

---
+++ java/src/net/ahlawat/Integrator.java
@@ -0,0 +1,11 @@
+package net.ahlawat;
+
+/**
+ * Created by IntelliJ IDEA.
+ * User: pranay
+ * Date: Jan 30, 2010
+ * Time: 12:57:17 AM
+ * To change this template use File | Settings | File Templates.
+ */
+public class Integrator {
+}
---
+++ java/src/net/ahlawat/Main.java
@@ -0,0 +1,11 @@
+package net.ahlawat;
+
+/**
+ * Created by IntelliJ IDEA.
+ * User: pranay
+ * Date: Jan 30, 2010
+ * Time: 1:15:21 AM
+ * To change this template use File | Settings | File Templates.
+ */
+public class Main {
+}
